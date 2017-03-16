df6999e8b0222a3e84457c1b25b0ddcf9679dba6
VascoUP <vasco_mag_pereira@hotmail.com>
2017-03-16 08:27:46 +0000

    Added database classes

---
+++ YM/database/tables/ym.sql
@@ -0,0 +1,50 @@
+/*
+		USERS
+*/
+CREATE TABLE User {
+
+}
+
+CREATE TABLE Administrator {
+
+}
+
+CREATE TABLE AuthenticatedUser {
+
+}
+
+CREATE TABLE Coordinator {
+
+}
+
+
+/*
+		PROJECTS
+*/
+CREATE TABLE Project {
+
+}
+
+CREATE TABLE Tag {
+
+}
+
+CREATE TABLE Forum {
+
+}
+
+CREATE TABLE Thread {
+
+}
+
+CREATE TABLE Comment {
+
+}
+
+CREATE TABLE Iteration {
+
+}
+
+CREATE TABLE Task {
+
+}
