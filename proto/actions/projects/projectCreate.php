dffd940803e64b36ff1cd2a4cb0f03679970748a
saracouto1318 <saracouto1318@hotmail.com>
2017-04-18 20:46:02 +0100

    changes

--- proto/actions/projects/projectCreate.php
+++ proto/actions/projects/projectCreate.php
@@ -2,7 +2,7 @@
 	include_once('../../config/init.php');
 	include_once($BASE_DIR .'database/project.php');  
 
-	if (!$_POST['projName'] || !$_POST['overview'] || !$_POST['tags'] || !$_POST['access') {
+	if (!$_POST['projName'] || !$_POST['overview'] || !$_POST['tags'] || !$_POST['access']) {
 		$_SESSION['error_messages'][] = 'All fields are mandatory';
 		$_SESSION['form_values'] = $_POST;
 		header("Location: $BASE_URL" . 'pages/project/general/projectCreate.php');
--- proto/templates_c/17c6def2956807b92d5d94e96e7886af39d0e9d7.file.header.tpl.php
+++ proto/templates_c/17c6def2956807b92d5d94e96e7886af39d0e9d7.file.header.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2017-04-18 18:00:41
+<?php /* Smarty version Smarty-3.1.15, created on 2017-04-18 20:37:56
          compiled from "/opt/lbaw/lbaw1654/public_html/proto/templates/common/header.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:91416728158f258ac37fd07-36169235%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '17c6def2956807b92d5d94e96e7886af39d0e9d7' => 
     array (
       0 => '/opt/lbaw/lbaw1654/public_html/proto/templates/common/header.tpl',
-      1 => 1492534781,
+      1 => 1492544208,
       2 => 'file',
     ),
   ),
@@ -102,9 +102,9 @@ javascript/dropdownUser.js"></script>
 							<li>
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User<span class="caret"></span></a>
 								<ul class="dropdown-menu">
-									<li><a id="drop">Profile</a></li>
+									<li><a href="https://gnomo.fe.up.pt/~lbaw1654/proto/pages/profile/profileUserOverview.php" role="button" id="drop">Profile</a></li>
 									<li><a id="drop">Projects</a></li>
-									<li><a id="drop">Edit Profile</a></li>
+									<li><a href="https://gnomo.fe.up.pt/~lbaw1654/proto/pages/profile/editProfile.php" role="button" id="drop">Edit Profile</a></li>
 									<li><a href="https://gnomo.fe.up.pt/~lbaw1654/proto/actions/users/logout.php" role="button" id="drop" class="logout">Logout</a></li>
 								</ul>
 							</li>
