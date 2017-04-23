402296909e0d5dbce146d97bce92e24e6471061e
VascoUP <vasco_mag_pereira@hotmail.com>
2017-04-23 15:00:05 +0100

    All templates are completed but not optimized

--- proto/pages/users/register.php
+++ proto/pages/users/register.php
@@ -3,6 +3,5 @@
 	
 	$smarty->display($BASE_DIR .'templates/common/headerLoginRegister.tpl');
 	$smarty->display($BASE_DIR .'templates/users/register.tpl');
-
 	$smarty->display($BASE_DIR .'templates/common/footer.tpl'); 
 ?>
