4cd77ad25f1b8c50cdae8178e7989d9b2052bf13
saracouto1318 <saracouto1318@hotmail.com>
2017-04-24 22:40:53 +0100

    photo

--- proto/actions/profile/editProfile.php
+++ proto/actions/profile/editProfile.php
@@ -12,9 +12,35 @@
 	 updateEmail($_POST['email']);  
   else if($_POST['upload'] || $_POST['uploadCV'])
   {
-	  if($_POST['upload'])
+	  if($_POST['upload']){
+		  if (isset($_FILES['upload'])) {
+			$profilePicName = $_FILES['upload']['name'];
+			$profilePicTempName = $_FILES['upload']['tmp_name'];
+			if(isset($profilePicName)) {
+				if(!empty($profilePicName)) {
+					$extension = strtolower(substr($profilePicName, strpos($profilePicName, '.')));
+					$fileName = "images/assets/$username$extension";
+					$thumbnailName = "images/assets/$username$extension";
+					move_uploaded_file($profilePicTempName, $fileName);
+					if($extension == '.png')
+						$originalImage = imagecreatefrompng($fileName);
+					else 
+						$originalImage = imagecreatefromjpeg($fileName);
+					$width = imagesx($originalImage);
+					$height = imagesy($originalImage);
+					$square = min($width, $height);
+					//Create 200x200 thumbnail
+					$thumbnail = imagecreatetruecolor(200, 200); 
+					imagecopyresized($thumbnail, $originalImage, 0, 0, ($width>$square)?($width-$square)/2:0, ($height>$square)?($height-$square)/2:0, 200, 200, $square, $square);
+					if($extension == '.png')
+						imagepng($thumbnail, $thumbnailName);
+					else
+						imagejpeg($thumbnail, $thumbnailName);
+				}
+			}
+		}
 		  updatePhoto($_POST['upload']);
-	  
+	  }
 	  if($_POST['uploadCV'])
 		  updateCurriculum($_POST['uploadCV']);
   }
