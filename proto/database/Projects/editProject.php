173c1de551aca1a192caa7960f4c13645175fcdf
VascoUP <vasco_mag_pereira@hotmail.com>
2017-04-24 16:02:24 +0100

    Added exception handlers for databse consulting

--- proto/database/Projects/editProject.php
+++ proto/database/Projects/editProject.php
@@ -1,56 +1,71 @@
 <?
 	include($BASE_DIR .'database/sanitize.php');
 
-  function updateProjName($name, $id){
+  	function updateProjName($name, $id){
 		echo "<script>console.log( '2' );</script>";
 
 		$secureName = sanitize($name);
 
-		global $conn;
-		$stmt = $conn->prepare("UPDATE Project
-								SET name = ?
-								WHERE projectID = ?");		
-		$stmt->execute(array($secureName, $id));
+		try {
+			global $conn;
+			$stmt = $conn->prepare("UPDATE Project
+			SET name = ?
+			WHERE projectID = ?");		
+			$stmt->execute(array($secureName, $id));
+		} catch(Exception $e) {
+			return $e->getMessage();
+		}
 
 		echo "<script>console.log( '3' );</script>";
-  }
+	}
   
-  function updateOverview($overview, $id){		
+ 	function updateOverview($overview, $id){		
 		$secureOverview = sanitize($overview);
-
-		global $conn;
-		$stmt = $conn->prepare("UPDATE Project
+		try {
+			global $conn;
+			$stmt = $conn->prepare("UPDATE Project
 								SET description = ?
 								WHERE projectID = ?");
-    $stmt->execute(array($secureOverview, $id));
-  }
+			$stmt->execute(array($secureOverview, $id));
+		} catch(Exception $e) {
+			return $e->getMessage();
+		}
+  	}
   
-  function updateOverview($id){
-		global $conn;
-		$stmt = $conn->prepare("SELECT access
-								FROM Project
-								WHERE projectID = ?");
-		$stmt->execute(array($id));
+  	function updateOverview($id){		
+		try {
+			global $conn;
+			$stmt = $conn->prepare("SELECT access
+									FROM Project
+									WHERE projectID = ?");
+			$stmt->execute(array($id));
 
-		$result = $stmt->fetchAll();
-		$access = $result['0']['access'];
-		
-		if($access == 'true')
-			$newAccess = 'false';
-		else
-			$newAccess = 'true';
-		
-		$stmt = $conn->prepare("UPDATE Project
-								SET access = ?
-								WHERE projectID = ?");
-    $stmt->execute(array($newAccess, $id));
-  }
+			$result = $stmt->fetchAll();
+			$access = $result['0']['access'];
+			
+			if($access == 'true')
+				$newAccess = 'false';
+			else
+				$newAccess = 'true';
+			
+			$stmt = $conn->prepare("UPDATE Project
+									SET access = ?
+									WHERE projectID = ?");
+			$stmt->execute(array($newAccess, $id));
+		} catch(Exception $e) {
+			return $e->getMessage();
+		}
+  	}
   
-  function deleteProject($id){
-		global $conn;
-		$stmt = $conn->prepare("DELETE
-								FROM Project
-								WHERE projectID = ?");
-    $stmt->execute(array($id));
-  }
+  	function deleteProject($id){
+		try {
+			global $conn;
+			$stmt = $conn->prepare("DELETE
+									FROM Project
+									WHERE projectID = ?");
+			$stmt->execute(array($id));
+		} catch(Exception $e) {
+			return $e->getMessage();
+		}
+  	}
 ?>
--- proto/database/Projects/projects.php
+++ proto/database/Projects/projects.php
@@ -1,56 +1,81 @@
 <?php
 	include_once($BASE_DIR .'database/Users/userInformation.php');
 	include($BASE_DIR .'database/sanitize.php');
-	
-  function createProject($projName, $description, $access) {
+
+	function createProject($projName, $description, $access) {
 		$secureName = sanitize($projName);
 		$secureDescription = sanitize($description);
 
-    global $conn;
+		try {
+			global $conn;
+			$stmt = $conn->prepare("UPDATE Project
+																SET name = ?
+																WHERE projectID = ?");		
+			$stmt->execute(array($secureName, $id));
+		} catch(Exception $e) {
+			return $e->getMessage();
+		}
 
-		if($access === 'public')
-			$typeAccess = 'true';
-		else
-			$typeAccess = 'false';
-		
-		$stmt = $conn->prepare("INSERT INTO Project(name, description, access)
-									VALUES (:name, :description, :access)");						
-		$stmt->bindParam(':name', $secureName);
-		$stmt->bindParam(':description', $secureDescription);
-		$stmt->bindParam(':access', $typeAccess);
-		$stmt->execute();	
+		try {
+			global $conn;
+
+			if($access === 'public')
+				$typeAccess = 'true';
+			else
+				$typeAccess = 'false';
+
+			$stmt = $conn->prepare("INSERT INTO Project(name, description, access)
+																VALUES (:name, :description, :access)");						
+			$stmt->bindParam(':name', $secureName);
+			$stmt->bindParam(':description', $secureDescription);
+			$stmt->bindParam(':access', $typeAccess);
+			$stmt->execute();	
+
+		} catch(Exception $e) {
+			return $e->getMessage();
+		}
 		
 		insertProjCoord($projName);
-  }
-  
-  function getProjectID($proj){
-		global $conn;
-    $stmt = $conn->prepare("SELECT projectID 
-                            FROM Project
-                            WHERE name = ?");
-							
-    $stmt->execute(array($proj));
-		$result = $stmt->fetchAll();
-    return $result['0']['projectid'];
-  }
-  
-  function insertProjCoord($proj){
-		global $conn;
-		$projStatus = "working";
-		$startDate = date('Y-m-d');
-		$userID = getUserID($_SESSION['username']);
-		$secureUsrID = sanitize($userID);
-		$projID = getProjectID($proj);
-		
-		$stmt = $conn->prepare("INSERT INTO ProjectCoordinator(userID, projectID, startDate, projectStatus)
-								VALUES (:userID, :projectID, :startDate, :projectStatus)");
-		$stmt->bindParam(':userID', $secureUsrID);
-		$stmt->bindParam(':projectID', $projID);
-		$stmt->bindParam(':startDate', $startDate);
-		$stmt->bindParam(':projectStatus', $projStatus);
-		$stmt->execute();
-		
-		$stmt = $conn->prepare("UPDATE UserSite SET type='coordinator' WHERE userID = ?");
-		$stmt->execute(array($secureUsrID));
-  }
+	}
+
+	function getProjectID($proj){
+		try {
+			global $conn;
+			$stmt = $conn->prepare("SELECT projectID 
+																FROM Project
+																WHERE name = ?");
+			$stmt->execute(array($proj));
+			$result = $stmt->fetchAll();
+		} catch(Exception $e) {
+			return $e->getMessage();
+		}
+
+		return $result['0']['projectid'];
+	}
+
+	function insertProjCoord($proj){
+		try {
+			global $conn;
+			$projStatus = "working";
+			$startDate = date('Y-m-d');
+			$userID = getUserID($_SESSION['username']);
+			$secureUsrID = sanitize($userID);
+			$projID = getProjectID($proj);
+
+			$stmt = $conn->prepare("INSERT INTO ProjectCoordinator(userID, projectID, startDate, projectStatus)
+																VALUES (:userID, :projectID, :startDate, :projectStatus)");
+			$stmt->bindParam(':userID', $secureUsrID);
+			$stmt->bindParam(':projectID', $projID);
+			$stmt->bindParam(':startDate', $startDate);
+			$stmt->bindParam(':projectStatus', $projStatus);
+			$stmt->execute();
+
+			$stmt = $conn->prepare("UPDATE UserSite 
+																SET type='coordinator' 
+																WHERE userID = ?");
+			$stmt->execute(array($secureUsrID));
+		} catch(Exception $e) {
+			return $e->getMessage();
+		}
+	}
 ?>
