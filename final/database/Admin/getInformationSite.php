46184850ccb172a4e9f85ffb604ab190c88ccd63
saracouto1318 <saracouto1318@hotmail.com>
2017-05-02 18:25:48 +0100

    url changes

--- final/database/Admin/getInformationSite.php
+++ final/database/Admin/getInformationSite.php
@@ -50,7 +50,8 @@ function searchProjects($name){
 								OR (to_tsvector('english', Tag.name) @@ to_tsquery('english', ?)
 								OR Tag.name ILIKE '%?%'
 								AND TagProject.tagID = Tag.tagID
-								AND Project.projectID = TagProject.projectID)");
+								AND Project.projectID = TagProject.projectID)
+								LIMIT 10");
 		$stmt->execute(array($name, $name, $name, $name));
 	} catch(Exception $e) {
 		return $e->getMessage();
