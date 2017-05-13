640f85fcc958821abc7a9dfdfb6e1bafbc367ee9
saracouto1318 <saracouto1318@hotmail.com>
2017-05-13 15:40:34 +0100

    changes

--- final/actions/general/search.php
+++ final/actions/general/search.php
@@ -7,7 +7,5 @@ $name = $_POST['search'];
 $users = searchUsers($name);
 $projects = searchProjects($name);
 
-$_SESSION['success_messages'][] = 'Search done successfully';  
-header('Location: ' .$BASE_URL.'pages/general/searchResults.php?users='.$users.'&proj='.$projects);
 
 ?>
